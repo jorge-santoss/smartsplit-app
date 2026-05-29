9fcee0802a5a6f084e6fdc69bab7106ede5ba7ec
Jorge Santos <jorge.santos@laplateforme.io>
2026-05-29 15:22:29 +0200

    add settlement API functions for creating and listing settlements by household

---
+++ frontend/src/api/settlementApi.js
@@ -0,0 +1,9 @@
+import client from './client';
+
+export const create = (householdId, data) => {
+  return client.post(`/api/households/${householdId}/settlements`, data);
+};
+
+export const listByHousehold = (householdId) => {
+  return client.get(`/api/households/${householdId}/settlements`);
+};
