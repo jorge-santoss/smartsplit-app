9cfd759cf2e5b58905e623ca3903cb59d385e8ba
Jorge Santos <jorge.santos@laplateforme.io>
2026-06-08 16:04:40 +0200

    Add getSummary API endpoint to retrieve household balance summaries

--- frontend/src/api/balanceApi.js
+++ frontend/src/api/balanceApi.js
@@ -3,3 +3,7 @@ import client from './client';
 export const getBalances = (householdId) => {
   return client.get(`/api/households/${householdId}/balances`);
 };
+
+export const getSummary = () => {
+  return client.get('/api/households/balances/summary');
+};
